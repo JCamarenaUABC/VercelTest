7d43635e398e4a9b0a592c3236c56324e18f19fe
JCamarenaUABC <69605148+JCamarenaUABC@users.noreply.github.com>
2025-10-22 15:42:46 -0700

    Iniciacion

---
+++ apitest.py
@@ -0,0 +1,20 @@
+﻿#from flask import Flask, jsonify
+#import sqlite3
+import os
+import requests
+from flask import Flask, jsonify, request, render_template
+import pyodbc
+from bs4 import BeautifulSoup
+#from flask_cors import CORS  #INSTALAR
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)  
+
+
+@app.route("/hello", methods=['GET'])
+def hello():
+    return jsonify({"mensaje": "Hello JFCM"}) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
